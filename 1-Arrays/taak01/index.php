598abceb200bd1f1749d202982d902348bcda6fc
MaxMiddendorp <2100773@talnet.nl>
2020-06-10 12:38:34 +0200

    1-Arrays af

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Taak 1.1 </title>
+</head>
+<body>
+<?php
+    $studentvoor['Max'] = "Max";
+    $studentachter['Max'] = "Middendorp";
+    $studentleef['Max'] = "17";
+    $studentklas['Max'] = "9D";
+    echo "Gegevens". $studentvoor['Max'] . ":  " . "Voornaam:" . $studentvoor['Max'] . "  Achternaam:" . $studentachter['Max'] . "  Leeftijd:" . $studentleef['Max'] . "  Klas:" . $studentklas['Max'];
+    ?>
+</body>
+</html>
